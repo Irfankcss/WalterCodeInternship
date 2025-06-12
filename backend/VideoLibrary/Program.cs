a7bc97771cabe50cb5aba2fc97219983497b2f02
Irfankcss <114685092+Irfankcss@users.noreply.github.com>
2025-06-12 11:48:01 +0200

    konekcija na bazu
    Kreiran dbcontext i napisan connectionstring za bazu. Napravljen privremeni kontroller za testiranje konekcije

--- backend/VideoLibrary/Program.cs
+++ backend/VideoLibrary/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using VideoLibrary;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(10, 5, 0))
+    ));
+
 
 var app = builder.Build();
 
